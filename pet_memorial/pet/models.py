e3c6747160bf680605cf9242a618a69c1b0b9c5f
mkstarr <starrtennis@gmail.com>
2022-11-29 14:46:23 -0800

    For laptop work. Standard commit. Debugged some errors for homepage, but not for pet_profile.html. Current error: 404 Page not found for pet_details searching at http://127.0.0.1:8000/pet/slug though I don't think that is the actual link url I input. Not sure what's wrong.

--- pet_memorial/pet/models.py
+++ pet_memorial/pet/models.py
@@ -4,15 +4,14 @@ from django.utils.text import slugify
 class Pet(models.Model):
     name = models.CharField(max_length=255)
     age = models.IntegerField()
-    slug = models.SlugField(max_length=100, unique=True, default=save())
+    slug = models.SlugField(allow_unicode=True, max_length=100, unique=True, default="")
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        self.description_html = misaka.html(self.description)
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse("pet_details", kwargs={"slug": self.slug})
+        return reverse("pet:pet_details", kwargs={"slug": self.slug})
